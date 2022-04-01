5f297ddc01701eb939582a437c98ea2436ea297f
zohrashaik-kenzie <zohra.shaik@kenzie.academy>
2022-03-31 23:48:19 -0500

    Changed customer mappings

--- src/main/java/com/spring/dynamodb/controller/CustomerController.java
+++ src/main/java/com/spring/dynamodb/controller/CustomerController.java
@@ -6,23 +6,38 @@ import com.spring.dynamodb.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLDataException;
+
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
         @Autowired
         private CustomerRepository customerRepository;
 
-        @PostMapping("/add/customer")
-        public Customer saveCustomer(@RequestBody Customer customer) {
+    /**
+     *
+     * @param customer
+     * @return the inserted customer, customerId
+     *
+     * @throws SQLDataException
+     */
+        @PostMapping("/add")
+        public Customer saveCustomer(@RequestBody Customer customer) throws SQLDataException {
             return customerRepository.saveCustomer(customer);
         }
 
-        @GetMapping("/get/customer/{id}")
+    /**
+     *
+     * @param customerId
+     * @return
+     */
+    @GetMapping("/get/{id}")
         public Customer getCustomerById(@PathVariable("id") String customerId) {
             return customerRepository.getCustomerById(customerId);
         }
 
-        @DeleteMapping("/delete/customer/{id}")
+        @DeleteMapping("/delete/{id}")
         public String deleteCustomerById(@PathVariable("id") String customerId) {
             return customerRepository.deleteCustomerById(customerId);
         }
