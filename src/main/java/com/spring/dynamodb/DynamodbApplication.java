41e34bcc866c5632e3ea78a22de8e34775d2a3ad
zohrashaik-kenzie <zohra.shaik@kenzie.academy>
2022-05-19 14:47:32 -0500

    Week 16 - Spring Caching

--- src/main/java/com/spring/dynamodb/DynamodbApplication.java
+++ src/main/java/com/spring/dynamodb/DynamodbApplication.java
@@ -2,8 +2,11 @@ package com.spring.dynamodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DynamodbApplication {
 
 	public static void main(String[] args) {
